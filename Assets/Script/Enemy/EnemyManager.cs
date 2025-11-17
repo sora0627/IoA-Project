85bfe8c1f0520d06541d31eb12d1945ec4b4e1f5
sora0627 <pcsorairo@gmail.com>
2025-11-17 16:48:59 +0900

    Add　いろいろ追加

--- Assets/Script/Enemy/EnemyManager.cs
+++ Assets/Script/Enemy/EnemyManager.cs
@@ -1,6 +1,7 @@
 using Cards;
 using System.Collections;
 using System.Collections.Generic;
+using Systems;
 using UnityEngine;
 
 namespace Enemy
@@ -9,6 +10,8 @@ namespace Enemy
     {
         [SerializeField]
         public List<CardData> hands;
+
+        private bool isDraw = false;
 
         // Start is called before the first frame update
         void Start()
@@ -19,12 +22,35 @@ namespace Enemy
         // Update is called once per frame
         void Update()
         {
+            if (GameManager.instance.IsPlayerTurn) return;
 
+            if (GameManager.instance.IsSelect)
+            {
+                if (!isDraw)
+                {
+                    isDraw = true;
+                    CardManager.instance.DrawCard(hands);
+                }
+            }
+
+            if (GameManager.instance.IsSet)
+            {
+
+            }
         }
 
         public void UseHand(int index)
         {
             hands.RemoveAt(index);
+            GameManager.instance.IsSet = true;
+        }
+
+        void TurnEnd()
+        {
+            Debug.Log("PlayerTurn");
+            GameManager.instance.IsSelect = true;
+            GameManager.instance.IsPlayerTurn = true;
+            isDraw = false;
         }
     }
 }
